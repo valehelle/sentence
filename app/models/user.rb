90879a9a2ee16911ea975cf80ff33019e038c7c7
hazmi irfan <hazmi@lelong.com.my>
2017-11-08 16:26:12 +0800

    add nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
+  before_create :set_nickname
   include DeviseTokenAuth::Concerns::User
+
+    private
+      def set_nickname
+        self.nickname = "#{self.email[/^[^@]+/]}#{SecureRandom.hex(1)}"
+      end
 end
